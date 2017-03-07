ae8907e5af48b99ccb05f6b211ba0cc7c3edcb57
xiaopan <986872497@qq.com>
2017-03-07 22:56:12 +0800

    Initial commit

---
+++ app/src/main/java/com/scoot/coolweather/db/Province.java
@@ -0,0 +1,37 @@
+package com.scoot.coolweather.db;
+
+import android.os.Parcelable;
+
+/**
+ * Created by Scoot on 2017/3/7.
+ */
+
+public class Province  {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
